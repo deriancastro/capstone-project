9a3f482de4c31248ca2dbbec3899063b0edf1868
Derian Castro <deriancastro@hotmail.com>
2021-06-06 00:28:17 +0200

    Add a photo

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -2,6 +2,7 @@ import styled from 'styled-components/macro'
 import PropTypes from 'prop-types'
 import Header from '../components/Header'
 import Button from '../components/Button'
+import judo from '../img/judo.jpg'
 
 DetailPage.propTypes = {
   pageName: PropTypes.string,
@@ -12,7 +13,9 @@ export default function DetailPage({ toTutorialPage, pageName }) {
   return (
     <PageWrapper>
       <Header>{pageName}</Header>
-      <div></div>
+      <Container>
+        <img src={judo}></img>
+      </Container>
       <Nav>
         <Button onClick={toTutorialPage} isActive={false}>
           Back
@@ -24,11 +27,19 @@ export default function DetailPage({ toTutorialPage, pageName }) {
 
 const PageWrapper = styled.div`
   display: grid;
-  grid-template-rows: 60px auto 40px;
-  gap: 3px;
+  grid-template-rows: 60px auto 50px;
   height: 100vh;
 `
 const Nav = styled.nav`
   display: grid;
   box-shadow: 3px #0003;
+`
+const Container = styled.section`
+  display: grid;
+  align-content: center;
+  margin: 0 auto;
+
+  img {
+    border-radius: 20px;
+  }
 `
